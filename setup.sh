a1b8be3b8220af6037309ff5ac95a5e66af05ef0
Creaous <68230846+Creaous@users.noreply.github.com>
2024-09-21 22:17:32 +0930

    add reboot message

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-read -p "Are you sure you want to run this script? [y/N] " -n 1 -r
+read -p "Are you sure you want to run this script? [Y/n] " -n 1 -r REPLY
+REPLY=${REPLY:-Y}
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
@@ -17,6 +18,7 @@ sudo useradd viewer
 sudo mkdir -p /home/viewer/.vnc
 sudo mkdir -p /home/viewer/.config/i3
 sudo cp -r /tmp/bivnc/home/viewer/* /home/viewer
+sudo cp -r /tmp/bivnc/home/viewer/.config/i3/* /home/viewer/.config/i3
 sudo cp -r /tmp/bivnc/etc/sddm.conf.d/* /etc/sddm.conf.d
 sudo chmod -R 755 /home/viewer
 sudo chown -R viewer:viewer /home/viewer
@@ -24,4 +26,14 @@ sudo chmod 700 /home/viewer/start.sh
 sudo chmod +x /home/viewer/start.sh
 
 echo The script *should* be finished now.
-echo You can now reboot your system.
+
+read -p "Would you like to reboot your system? [Y/n] " -n 1 -r REPLY
+REPLY=${REPLY:-Y}
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo reboot
+else
+    echo "Exiting script..."
+    exit 1
+fi
