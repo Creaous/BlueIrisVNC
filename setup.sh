87ac34025f2286c5f785aaa0feb7aef275d30d09
Creaous <68230846+Creaous@users.noreply.github.com>
2024-09-21 22:13:53 +0930

    add confirmation message

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+read -p "Are you sure you want to run this script? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Running script..."
+else
+    echo "Exiting script..."
+    exit 1
+fi
+
 cd /tmp
 git clone https://github.com/Creaous/BlueIrisVNC.git /tmp/bivnc
 sudo apt-get install git wait-for-it tigervnc-viewer feh xorg sddm
@@ -11,3 +22,6 @@ sudo chmod -R 755 /home/viewer
 sudo chown -R viewer:viewer /home/viewer
 sudo chmod 700 /home/viewer/start.sh
 sudo chmod +x /home/viewer/start.sh
+
+echo The script *should* be finished now.
+echo You can now reboot your system.
