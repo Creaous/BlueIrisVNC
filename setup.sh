5715a7054ba8b8bf8feb92515dba34c61ffb19f2
Creaous <68230846+Creaous@users.noreply.github.com>
2024-09-21 22:10:28 +0930

    fix script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get install git wait-for-it tigervnc-viewer feh xorg sddm
 sudo useradd viewer
 sudo mkdir -p /home/viewer/.vnc
 sudo mkdir -p /home/viewer/.config/i3
-sudo cp -r /tmp/bivnc/home/viewer /home/viewer
-sudo cp -r /tmp/bivnc/etc/sddm.conf.d /etc/sddm.conf.d
+sudo cp -r /tmp/bivnc/home/viewer/* /home/viewer
+sudo cp -r /tmp/bivnc/etc/sddm.conf.d/* /etc/sddm.conf.d
 sudo chmod 700 /home/viewer/start.sh
 sudo chmod +x /home/viewer/start.sh
